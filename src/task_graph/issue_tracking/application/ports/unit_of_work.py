8ba6c3a44d618121edee9cfdb5b47b293c838439
Goodsome <xlxmesh@gmail.com>
2026-04-11 12:14:20 +0800

    Add type hints to UnitOfWork __exit__ method

--- src/task_graph/issue_tracking/application/ports/unit_of_work.py
+++ src/task_graph/issue_tracking/application/ports/unit_of_work.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from types import TracebackType
 from task_graph.issue_tracking.domain.ports.issue_repository import IssueRepository
 from task_graph.shared.ports.event_bus import EventBus
 
@@ -20,7 +21,12 @@ class UnitOfWork(ABC):
         pass
 
     @abstractmethod
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
         pass
 
     @abstractmethod
